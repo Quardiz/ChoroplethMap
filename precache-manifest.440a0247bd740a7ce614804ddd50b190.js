c18fa953a05c0036ba43e5506146c985d0be86d0
Quardiz <quardiz@protonmail.com>
2020-09-03 19:27:15 +0200

    Updates

---
+++ precache-manifest.440a0247bd740a7ce614804ddd50b190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567357016ab263482c2b427bb087112f",
+    "url": "/ChoroplethMap/index.html"
+  },
+  {
+    "revision": "8c74e2560b7593d8f6fb",
+    "url": "/ChoroplethMap/static/css/main.f9578d3d.chunk.css"
+  },
+  {
+    "revision": "78c6508b22514e0d181f",
+    "url": "/ChoroplethMap/static/js/2.d9edfe42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChoroplethMap/static/js/2.d9edfe42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c74e2560b7593d8f6fb",
+    "url": "/ChoroplethMap/static/js/main.fbe66347.chunk.js"
+  },
+  {
+    "revision": "3d91eb99391c106e2ca2",
+    "url": "/ChoroplethMap/static/js/runtime-main.e8eddb54.js"
+  }
+]);
